cde356c29e03712b28f5b6d42f1927d648c1b6d4
TechGeekConnect Technology <techgeekconnect@gmail.com>
2025-07-27 11:33:01 +0530

    Create HelloWorld.sh

---
+++ HelloWorld.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#This is my first shell script program
+echo "Hello World!"
+
+
+number1=10
+number2=20
+
+add=`expr $number1 + $number2`
+echo "Addition of two number is:" $add
+echo $USER
+echo $PATH
